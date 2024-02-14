a215514c34cc03db04db2e0253953d5d427e1ab7
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-02-14 21:23:31 +0100

    modified:   src/components/navbar/Navbar.jsx modified:   src/components/navbar/navbar.css modified:   src/components/update/Update.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import "./navbar.css";
 
 export default function Navbar() {
+
+
   return (
     <>
       <div className="navbar">
         <div>Redux</div>
-        <div className="search-container">
-    <form action="/action_page.php">
-      <input type="text" placeholder="Search.." name="search" />
-      <button type="submit"><i className="fa fa-search"></i></button>
-    </form>
-  </div>
+        <div>
+          <form action="">
+            <input type="text" placeholder="Tyoe to search" />
+          </form>
+        </div>
         <div>Jakaza</div>
       </div>
     </>
--- src/components/update/Update.jsx
+++ src/components/update/Update.jsx
@@ -1,17 +1,30 @@
 import "./update.css";
 import Warning from "../warning/Warning";
+import { useState } from "react";
 export default function Update() {
+
+  const [name, setName] = useState('Name Here')
+
+  function handleFormSubmit(e){
+    e.preventDefault()
+
+    alert(name)
+  }
+
   return (
     <div className="update">
       <Warning message="This is a warning" />
 
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <label for="fname">First Name</label>
         <input
           type="text"
           id="fname"
           name="firstname"
           placeholder="Your name.."
+          onChange={(e)=>{
+            setName(e.target.value)
+          }}
         />
 
         <label for="lname">Last Name</label>
